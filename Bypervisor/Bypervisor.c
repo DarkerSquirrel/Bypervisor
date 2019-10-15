5c81192c1fdf801f6e88b5cecad6c3afa1ac25a1
0h90 <26379779+0h90@users.noreply.github.com>
2019-10-15 23:35:35 +1100

    Added a few typedefs and VT-x probing

--- Bypervisor/Bypervisor.c
+++ Bypervisor/Bypervisor.c
@@ -1,6 +1,70 @@
 #include "Bypervisor.h"
 
-NTSTATUS InitializeHypervisor()
+PVMM_CONTEXT BypervisorContext = NULL;
+
+BOOLEAN ProbeVtx()
 {
-    
+    NTSTATUS Status = FALSE;
+    int CpuIdOut[4];
+
+    __cpuid(CpuIdOut, 1);
+
+    if (!(CpuIdOut[2] & 0x20))
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "VT-x not supported.\n");
+        goto Exit;
+    }
+
+    unsigned long long FeatureControl = __readmsr(IA32_FEATURE_CONTROL);
+
+    // Check LOCK bit of IA32_FEATURE_CONTROL (b0)
+    if (!(FeatureControl & 0x1))
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Feature control MSR - Lock bit not set.\n");
+        goto Exit;
+    }
+
+    // Check Out-of-SMX operation of IA32_FEATURE_CONTROL (b2)
+    if (!((FeatureControl >> 2) & 0x1))
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "VT-x not supported outside of SMX.\n");
+        goto Exit;
+    }
+
+    Status = TRUE;
+
+Exit:
+    return Status;
+}
+
+NTSTATUS SetupVtx()
+{
+    NTSTATUS Status = STATUS_UNSUCCESSFUL;
+
+    if (!ProbeVtx())
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "VT-x probe failed.\n");
+        goto Exit;
+    }
+
+    BypervisorContext = (PVMM_CONTEXT)ExAllocatePoolWithTag(
+        NonPagedPoolNx, 
+        sizeof(VMM_CONTEXT), 
+        BYPERVISOR_NONPAGEDPOOL_TAG);
+
+    if (BypervisorContext == NULL)
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Failed to allocate memory.\n");
+        goto Exit;
+    }
+
+    BypervisorContext->ProcessorsInitialised = 0;
+    BypervisorContext->SystemProcessorCount = KeQueryActiveProcessorCountEx(ALL_PROCESSOR_GROUPS);
+
+    ASSERT(BypervisorContext->SystemProcessorCount > 0);
+
+    Status = STATUS_SUCCESS;
+
+Exit:
+    return Status;
 }
--- Bypervisor/Bypervisor.h
+++ Bypervisor/Bypervisor.h
@@ -4,7 +4,66 @@
 #include <intrin.h>
 #include "ia32.h"
 
+#define BYPERVISOR_NONPAGEDPOOL_TAG 'ppnB'
+#define BYPERVISOR_PAGEDPOOL_TAG    'ppyB'
+
+typedef VMCS* PVMCS;
+typedef VMX_MSR_BITMAP* PMSRBMAP;
+
 typedef struct _VMM_CONTEXT
 {
-
+    UINT32 SystemProcessorCount;
+    UINT32 ProcessorsInitialised;
 } VMM_CONTEXT, *PVMM_CONTEXT;
+
+typedef struct _REG_CAPTURE
+{
+    UINT64 rax;
+    UINT64 rbx;
+    UINT64 rcx;
+    UINT64 rdx;
+    UINT64 rsi;
+    UINT64 rdi;
+    UINT64 rbp;
+    UINT64 rsp;
+
+    UINT64 r8;
+    UINT64 r9;
+    UINT64 r10;
+    UINT64 r11;
+    UINT64 r12;
+    UINT64 r13;
+    UINT64 r14;
+    UINT64 r15;
+
+    SEGMENT_SELECTOR cs;
+    SEGMENT_SELECTOR ds;
+    SEGMENT_SELECTOR es;
+    SEGMENT_SELECTOR fs;
+    SEGMENT_SELECTOR gs;
+    SEGMENT_SELECTOR ss;
+
+    UINT64 dr0;
+    UINT64 dr1;
+    UINT64 dr2;
+    UINT64 dr3;
+    UINT64 dr6;
+    UINT64 dr7;
+} REG_CAPTURE, *PREG_CAPTURE;
+
+typedef struct _REG_SPEC_CAPTURE
+{
+    UINT64 cr0;
+    UINT64 cr2;
+    UINT64 cr3;
+    UINT64 cr4;
+} REG_SPEC_CAPTURE, *PREG_SPEC_CAPTURE;
+
+typedef struct _VMM_PER_PROC_CONTEXT
+{
+    BOOLEAN HasLaunched;
+    PVMCS pVmcs;
+    PVOID pPhysVmcs;
+    PMSRBMAP pMsrBitmap;
+    PVOID pPhysMsrBitmap;
+};
