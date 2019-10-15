6f616198e8ce4a61ae87d4dd19b7a86ba6c7dea5
0h90 <26379779+0h90@users.noreply.github.com>
2019-10-16 01:45:48 +1100

    Added a million structs and typedefs. Need to declare EPT struct properly

--- Bypervisor/Bypervisor.h
+++ Bypervisor/Bypervisor.h
@@ -7,34 +7,49 @@
 #define BYPERVISOR_NONPAGEDPOOL_TAG 'ppnB'
 #define BYPERVISOR_PAGEDPOOL_TAG    'ppyB'
 
-typedef VMCS* PVMCS;
+// 16 MB
+#define VMM_HOST_STACK_SIZE 0x1000000
+
+typedef VMCS*           PVMCS;
 typedef VMX_MSR_BITMAP* PMSRBMAP;
+typedef UINT64          REG64;
+typedef UINT32          REG32;
+typedef EPT_PML4*       PPML4;
+typedef PDPTE_64*       PPDPTE;
+typedef PDE_64*         PPDE;
+typedef PTE_64*         PPTE;
 
 typedef struct _VMM_CONTEXT
 {
-    UINT32 SystemProcessorCount;
-    UINT32 ProcessorsInitialised;
+    UINT32  SystemProcessorCount;
+    UINT32  ProcessorsInitialised;
+    PVOID   SystemDirectoryTableBase;
 } VMM_CONTEXT, *PVMM_CONTEXT;
+
+typedef struct _VMM_HOST_STACK
+{
+    UINT8   Stack[VMM_HOST_STACK_SIZE];
+} VMM_HOST_STACK, *PVMM_HOST_STACK;
 
 typedef struct _REG_CAPTURE
 {
-    UINT64 rax;
-    UINT64 rbx;
-    UINT64 rcx;
-    UINT64 rdx;
-    UINT64 rsi;
-    UINT64 rdi;
-    UINT64 rbp;
-    UINT64 rsp;
+    REG64 rax;
+    REG64 rbx;
+    REG64 rcx;
+    REG64 rdx;
+    REG64 rsi;
+    REG64 rdi;
+    REG64 rbp;
+    REG64 rsp;
 
-    UINT64 r8;
-    UINT64 r9;
-    UINT64 r10;
-    UINT64 r11;
-    UINT64 r12;
-    UINT64 r13;
-    UINT64 r14;
-    UINT64 r15;
+    REG64 r8;
+    REG64 r9;
+    REG64 r10;
+    REG64 r11;
+    REG64 r12;
+    REG64 r13;
+    REG64 r14;
+    REG64 r15;
 
     SEGMENT_SELECTOR cs;
     SEGMENT_SELECTOR ds;
@@ -43,12 +58,14 @@ typedef struct _REG_CAPTURE
     SEGMENT_SELECTOR gs;
     SEGMENT_SELECTOR ss;
 
-    UINT64 dr0;
-    UINT64 dr1;
-    UINT64 dr2;
-    UINT64 dr3;
-    UINT64 dr6;
-    UINT64 dr7;
+    REG64 dr0;
+    REG64 dr1;
+    REG64 dr2;
+    REG64 dr3;
+    REG64 dr6;
+    REG64 dr7;
+
+    REG64 rflags;
 } REG_CAPTURE, *PREG_CAPTURE;
 
 typedef struct _REG_SPEC_CAPTURE
@@ -59,11 +76,23 @@ typedef struct _REG_SPEC_CAPTURE
     UINT64 cr4;
 } REG_SPEC_CAPTURE, *PREG_SPEC_CAPTURE;
 
+typedef struct _EPT_PAGE_TABLE
+{
+    PPML4   Pml4;
+    PPDPTE  pPdpt;
+    PPDE    pPde;
+    PPTE    pPte;
+} EPT_PAGE_TABLE, *PEPT_PAGE_TABLE;
+
 typedef struct _VMM_PER_PROC_CONTEXT
 {
-    BOOLEAN HasLaunched;
-    PVMCS pVmcs;
-    PVOID pPhysVmcs;
-    PMSRBMAP pMsrBitmap;
-    PVOID pPhysMsrBitmap;
+    BOOLEAN          HasLaunched;
+    PVMCS            pVmcs;
+    PVOID            pPhysVmcs;
+    PMSRBMAP         pMsrBitmap;
+    PVOID            pPhysMsrBitmap;
+    REG_CAPTURE      RegisterCapture;
+    REG_SPEC_CAPTURE RegisterSpecCapture;
+    PEPT_PAGE_TABLE  pEpt;
+    PVOID            pPhysEpt;
 };
